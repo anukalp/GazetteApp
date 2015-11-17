eeb9061377d799b7adc3fb7636196010571051d6
anilgudigar <anil.guidgar@gmail.com>
2015-11-17 21:43:19 +0530

    BarCode Scanner zxing barcode API added.

--- Gazette_App/gazette/src/main/java/com/gazette/app/GazetteBarCodeScanActivity.java
+++ Gazette_App/gazette/src/main/java/com/gazette/app/GazetteBarCodeScanActivity.java
@@ -1,39 +1,18 @@
 package com.gazette.app;
 
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Vibrator;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.KeyEvent;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.gazette.app.utils.SharedPreferenceManager;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
 
-import me.dm7.barcodescanner.zbar.BarcodeFormat;
-import me.dm7.barcodescanner.zbar.Result;
-import me.dm7.barcodescanner.zbar.ZBarScannerView;
-
-public class GazetteBarCodeScanActivity extends GazetteBaseActivity implements
-        ZBarScannerView.ResultHandler {
-    private static final String FLASH_STATE = "FLASH_STATE";
-    private static final String AUTO_FOCUS_STATE = "AUTO_FOCUS_STATE";
-    private static final String SELECTED_FORMATS = "SELECTED_FORMATS";
-    private static final String CAMERA_ID = "CAMERA_ID";
-    private ZBarScannerView mScannerView;
-    private boolean mFlash;
-    private boolean mAutoFocus;
-    private ArrayList<Integer> mSelectedIndices;
-    private int mCameraId = -1;
-
-    static {
-        System.loadLibrary("iconv");
-    }
-
+public class GazetteBarCodeScanActivity extends GazetteBaseActivity {
+    private CaptureManager capture;
+    private CompoundBarcodeView barcodeScannerView;
+    private android.support.v7.widget.Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,14 +21,13 @@ public class GazetteBarCodeScanActivity extends GazetteBaseActivity implements
             // Cancel request if there is no rear-facing camera.
             return;
         }
-        // Create and configure the ImageScanner;
-        setupScanner();
-        // Create a RelativeLayout container that will hold a SurfaceView,
-        // and set it as the content of our activity.
         setContentView(R.layout.activity_gazette_bar_code_scan);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        mScannerView = (ZBarScannerView) findViewById(R.id.scannerview);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        barcodeScannerView = (CompoundBarcodeView) findViewById(R.id.zxing_barcode_scanner);
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
     }
 
     public boolean isCameraAvailable() {
@@ -57,76 +35,35 @@ public class GazetteBarCodeScanActivity extends GazetteBaseActivity implements
         return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 
-    public void setupScanner() {
-        mFlash = false;
-        mAutoFocus = true;
-        mSelectedIndices = null;
-        mCameraId = -1;
-        setupFormats();
-    }
-
-    public void setupFormats() {
-        List<BarcodeFormat> formats = new ArrayList<BarcodeFormat>();
-        if (mSelectedIndices == null || mSelectedIndices.isEmpty()) {
-            mSelectedIndices = new ArrayList<Integer>();
-            for (int i = 0; i < BarcodeFormat.ALL_FORMATS.size(); i++) {
-                mSelectedIndices.add(i);
-            }
-        }
-
-        for (int index : mSelectedIndices) {
-            formats.add(BarcodeFormat.ALL_FORMATS.get(index));
-        }
-        if (mScannerView != null) {
-            mScannerView.setFormats(formats);
-        }
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera(mCameraId);
-        mScannerView.setFlash(mFlash);
-        mScannerView.setAutoFocus(mAutoFocus);
+        capture.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mScannerView.stopCamera();
+        capture.onPause();
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putBoolean(FLASH_STATE, mFlash);
-        outState.putBoolean(AUTO_FOCUS_STATE, mAutoFocus);
-        outState.putIntegerArrayList(SELECTED_FORMATS, mSelectedIndices);
-        outState.putInt(CAMERA_ID, mCameraId);
+        capture.onSaveInstanceState(outState);
     }
 
     @Override
-    public void handleResult(Result rawResult) {
-        try {
-            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            // Vibrate for 500 milliseconds
-            v.vibrate(500);
-        } catch (Exception e) {
-            Log.i("Anil", "exception");
-        }
-        Log.i("Anil", "Contents = " + rawResult.getContents() + ", Format = " + rawResult.getBarcodeFormat().getName());
-        barcodeScannedConfimation(rawResult.getContents());
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
 
-    public boolean barcodeScannedConfimation(String barcode) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this,
-                    R.style.alertdialog_style);
-            builder.setTitle(getString(R.string.barcode_scanned));
-            builder.setMessage(barcode);
-            builder.setPositiveButton(getString(R.string.scan_again), null);
-            builder.setNegativeButton(getString(R.string.thats_right), null);
-            builder.show();
-            return false;
-    }
+
 }
