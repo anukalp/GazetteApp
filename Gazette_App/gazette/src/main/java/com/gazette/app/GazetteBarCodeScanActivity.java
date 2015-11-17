82d8b953514ac0bb5f89b234d50b3c521b34c635
anilgudigar <anil.guidgar@gmail.com>
2015-11-18 01:06:24 +0530

    BarCode Scan complete

--- Gazette_App/gazette/src/main/java/com/gazette/app/GazetteBarCodeScanActivity.java
+++ Gazette_App/gazette/src/main/java/com/gazette/app/GazetteBarCodeScanActivity.java
@@ -1,18 +1,26 @@
 package com.gazette.app;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
 
-import com.gazette.app.utils.SharedPreferenceManager;
-import com.journeyapps.barcodescanner.CaptureManager;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
 
+import java.util.List;
+
 public class GazetteBarCodeScanActivity extends GazetteBaseActivity {
-    private CaptureManager capture;
     private CompoundBarcodeView barcodeScannerView;
     private android.support.v7.widget.Toolbar mToolbar;
     private boolean flashToggle = false;
@@ -39,10 +47,16 @@ public class GazetteBarCodeScanActivity extends GazetteBaseActivity {
         }
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         barcodeScannerView = (CompoundBarcodeView) findViewById(R.id.zxing_barcode_scanner);
-        capture = new CaptureManager(this, barcodeScannerView);
-        capture.initializeFromIntent(getIntent(), savedInstanceState);
-        capture.decode();
+        triggerScan();
     }
 
     public boolean isCameraAvailable() {
@@ -50,36 +64,44 @@ public class GazetteBarCodeScanActivity extends GazetteBaseActivity {
         return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() != null) {
+                //barcodeScannerView.setStatusText(result.getText());
+                barcodeScannedConfimation(result.getText());
+            }
+            //Added preview of scanned barcode
+            ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
 
     @Override
     protected void onResume() {
         super.onResume();
-        capture.onResume();
+        barcodeScannerView.resume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        capture.onPause();
+        barcodeScannerView.pause();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        capture.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        capture.onSaveInstanceState(outState);
-    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
 
+    public void triggerScan() {
+        barcodeScannerView.decodeSingle(callback);
+    }
 
     /**
      * Check if the device's camera has a Flashlight.
@@ -99,6 +121,26 @@ public class GazetteBarCodeScanActivity extends GazetteBaseActivity {
             barcodeScannerView.setTorchOff();
             flashToggle = false;
         }
+    }
+
+    public boolean barcodeScannedConfimation(String barcode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this,
+                R.style.alertdialog_style);
+        builder.setTitle(getString(R.string.barcode_scanned));
+        builder.setMessage(barcode);
+        builder.setPositiveButton(getString(R.string.scan_again),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        triggerScan();
+                    }
+                });
+        builder.setNegativeButton(getString(R.string.thats_right), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                /* User clicked OK so do some stuff */
+            }
+        });
+        builder.show();
+        return false;
     }
 
 }
